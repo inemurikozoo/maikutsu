562cf5ac1e22a4bcb41d2ef2c0a2c18b4c28646f
inemurikozoo <100814669+inemurikozoo@users.noreply.github.com>
2022-07-23 12:03:39 +0000

    maikutsu save

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :admin,controllers: {
+    session: "admin/sessions"
+  }
+  devise_for :users,controllers: {
+    ragistrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
