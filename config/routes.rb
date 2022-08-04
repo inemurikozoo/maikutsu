b36e99adac3d2fc72d9700595c7280b5d992c9eb
inemurikozoo <100814669+inemurikozoo@users.noreply.github.com>
2022-08-04 20:12:44 +0000

    adin/user/index making

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,30 @@
 Rails.application.routes.draw do
-  
-  
+
+#管理者用deviseルート
   devise_for :admin, skip:[:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-  
+#会員用deviseルート
   devise_for :users, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  
+
+
     get root to: 'public/homes#top'
-  namespace :public do
     get 'users/show'
     get 'users/edit'
     get 'users/update'
     get 'users/exit'
-  end
-  namespace :public do
     get 'items/index'
     get 'items/show'
     get 'items/create'
     get 'items/edit'
     get 'items/update'
     get 'items/destroy'
-  end
-  namespace :public do
     get 'homes/top'
-    get 'homes/about'
-  end
+    get 'about' => 'public/homes#about', as: 'about'
+
   namespace :admin do
     get 'users/index'
     get 'users/show'
@@ -37,6 +33,6 @@ Rails.application.routes.draw do
     get 'items/index'
     get 'items/show'
   end
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,73 @@ Admin.create(
   email: "a@a",
   password: "aaaaaa"
 )
+
+Item.create!(
+  name: "にんじん",
+  preserve_method: "濡らさないように注意。袋やラップ、新聞紙にくるんで野菜室へ。しなびても水へ漬けたら復活！",
+  how_to_choose: "葉を落とした切り口がなるべく小さく、ひげ根が少ないもの。太過ぎないもの。",
+  limit: "野菜室で約1か月程度"
+  )
+
+Item.create!(
+  name: "キャベツ",
+  preserve_method: "芯をくりぬき、濡らしたペーパータオルを詰めておく。ポリ袋に入れる。",
+  how_to_choose: "芯の切り口が変色していないもの、全体的に色が濃いもののほうが良い。",
+  limit: "2週間程度"
+  )
+
+Item.create!(
+  name: "卵",
+  preserve_method: "パックに入れたままとがったほうを下にして保管する。腐る原因となるので洗ってはダメ。",
+  how_to_choose: "割ったときにプリっと黄身が盛り上がっているものは鮮度が高い。",
+  limit: "採卵後最長で57日。賞味期限は生で食べられる期限。"
+  )
+
+Item.create!(
+  name: "精肉",
+  preserve_method: "",
+  how_to_choose: "",
+  limit: ""
+  )
+
+Item.create!(
+  name: "ベーコン",
+  preserve_method: "",
+  how_to_choose: "",
+  limit: ""
+  )
+
+Item.create!(
+  name: "玉ねぎ",
+  preserve_method: "",
+  how_to_choose: "",
+  limit: ""
+  )
+
+Item.create!(
+  name: "牛乳",
+  preserve_method: "",
+  how_to_choose: "",
+  limit: ""
+  )
+
+Item.create!(
+  name: "水",
+  preserve_method: "",
+  how_to_choose: "",
+  limit: ""
+  )
+
+30.times do |n|
+  Item.create!(
+    name: "テスト#{n + 1}"
+    )
+end
+
+30.times do |n|
+  User.create!(
+    email: "test#{n + 1}@test.com",
+    name: "まいくつちゃん#{n + 1}",
+    password: "123456"
+  )
+end
