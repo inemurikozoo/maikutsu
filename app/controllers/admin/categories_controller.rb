b5c295457b2622add082143f99d81596e3413f61
inemurikozoo <100814669+inemurikozoo@users.noreply.github.com>
2022-08-11 16:29:40 +0000

    admin/items#edit making

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,8 +3,23 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def index
+    @categories = Category.all
+
   end
 
   def edit
+    @category = Category.find(params[:id])
+    
   end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update
+      flash[:notice] = "カテゴリを更新しました"
+      redirect_to admin_categories_path
+    else
+      render :edit
+    end
+  end
+
 end
