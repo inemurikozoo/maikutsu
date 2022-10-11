03807987a0cc4ef3a8cc07007b25f13158084c56
inemurikozoo <100814669+inemurikozoo@users.noreply.github.com>
2022-10-11 00:03:56 +0000

    fixfixfix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception #CSRF対策(クロスサイトリクエストフォージェリ)
-  before_action :authenticate_user!, except: [:top,:about,:guest_sign_in]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
